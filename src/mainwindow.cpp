3f8a37d90018a8c460c5d7cdfe3033f14d604b33
JackeyLea <1768478912@qq.com>
2020-03-29 19:05:20 +0800

    添加项目文件

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -13,3 +13,58 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::log(QString str)
+{
+    QString curTime = QDateTime::currentDateTime().toString("yy-MM-dd hh:mm:ss dddd");
+    qDebug()<<curTime+" "+str;
+    if(!logFilePath.isEmpty()){
+        QFile file(logFilePath);
+        if(file.open(QIODevice::Text|QIODevice::WriteOnly|QIODevice::Append)){
+            QTextStream writeOut(&file);
+            writeOut<<QString("【%1】 %2").arg(curTime).arg(str);
+        }
+        else{
+            qDebug()<<"Cannot open log file: "<<logFilePath;
+        }
+        file.close();
+    }
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    log("Trying to open something.");
+    QString filePath = QFileDialog::getOpenFileName(this,tr("Open"),QString(),QString("*.torrent\n*.metadata\n*"));
+    if(filePath.isEmpty()) {
+        log("Select nothing in Open() action.");
+        return;
+    }
+}
+
+void MainWindow::on_actionAdd_URLs_triggered()
+{
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    qApp->exit(0);
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QString msg=QString("<h1>Seagull</h1>"
+                        "<h2>下载软件</h2>"
+                        "<h3>支持http/https/ed2k/torrent格式、磁力搜索</h3>"
+                        "<h3>作者：幽弥狂</h3>");
+    QMessageBox::information(this,tr("About"),msg,QMessageBox::Ok);
+}
+
+void MainWindow::on_actionAbout_Qt_triggered()
+{
+    qApp->aboutQt();
+}
+
+void MainWindow::on_actionIndex_triggered()
+{
+
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDesktopServices>
+#include <QDateTime>
+#include <QMessageBox>
+#include <QUrl>
+#include <QString>
+#include <QDebug>
+#include <QFileDialog>
+#include <QFile>
+#include <QDir>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,10 +21,27 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    QString logFilePath;//日志文件详细路径
 
 private:
     Ui::MainWindow *ui;
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void log(QString str);
+
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionAdd_URLs_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionAbout_triggered();
+
+    void on_actionAbout_Qt_triggered();
+
+    void on_actionIndex_triggered();
 };
 #endif // MAINWINDOW_H
